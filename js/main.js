012b1684bbb4b3b0aab421a184be20dda7ef84dd
nobledm <40868595+nobledm@users.noreply.github.com>
2019-10-29 08:12:08 -0600

    completed viewport stylings except for largest breakpoint flex

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 const colorThief = new ColorThief();
 const img = document.querySelector(".linked-img");
-const palette = document.querySelector(".palette");
+const swatches = document.querySelector(".swatches");
 
 let error = document.querySelector(".error");
 let numSwatches = 4;
+
+/**
+ * Converts an RGB color into HEX
+ * @param {Number} r - Red color value from 0-255 
+ * @param {Number} g - Green color value from 0-255
+ * @param {Number} b - Blue color value from 0-255
+ */
+const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+  const hex = x.toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
+}).join('');
+
+/**
+ * Watches for errors on loading the image
+ */
+img.addEventListener("error", () => {
+  img.src = "./img/placeholder.jpg";
+  error.innerHTML =
+    "Could not find image. Check there are no typos. If it still doesn't work the origin site may be blocking access.";
+});
+
+/**
+ * Watches for new image loads and generates a swatches
+ */
+img.addEventListener("load", () => {
+  const colors = colorThief.getPalette(img, numSwatches);
+
+  // clear out any existing swatches
+  while (swatches.firstChild) {
+    swatches.removeChild(swatches.firstChild);
+  }
+
+  colors.reduce((swatches, rgb) => {
+    const color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    const swatch = document.createElement("div");
+
+    swatch.style.setProperty("--color", color);
+    swatch.setAttribute("hex", `${rgbToHex(rgb[0],rgb[1],rgb[2])}`);
+    swatches.appendChild(swatch);
+    return swatches;
+  }, swatches);
+});
 
 // watch for form submission and change img src attribute if valid
 document.querySelector(".img-submit").addEventListener("submit", e => {
@@ -16,28 +58,4 @@ document.querySelector(".img-submit").addEventListener("submit", e => {
   } else {
     img.src = e.target.elements.imgUrl.value;
   }
-});
-
-img.addEventListener("error", () => {
-  img.src = "./img/placeholder.jpg";
-  error.innerHTML =
-    "Could not find image. Check there are no typos. If it still doesn't work the origin site may be blocking access.";
-});
-
-img.addEventListener("load", () => {
-  const colors = colorThief.getPalette(img, numSwatches);
-
-  // clear out any existing swatches
-  while (palette.firstChild) {
-    palette.removeChild(palette.firstChild);
-  }
-
-  colors.reduce((palette, rgb) => {
-    const color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-    const swatch = document.createElement("div");
-
-    swatch.style.setProperty("--color", color);
-    palette.appendChild(swatch);
-    return palette;
-  }, palette);
 });
